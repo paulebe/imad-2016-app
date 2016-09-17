96c2861bf7584b8cf979352453bca44612a45518
Ebenezer Paul <paulebe4@gmail.com>
2016-09-18 02:19:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
- app.get('/paul',"Paul fILES IS REQUESTED AND WILL BE SEND ")
-
+app.get('/paul', function (req, res) {
+  res.sendFile("paul was requested");
+});
+ 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
